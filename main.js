643105e02c9ffb2c1f69d3328210b32312ac6abb
Dan Wilson <danielkeithw@gmail.com>
2020-03-28 23:56:34 -0700

    remove morgan

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-const yes = require("yes-https");
+const yesHttps = require("yes-https");
 const express = require("express");
-const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const bbcom = express();
+const debug = require("debug")("burnerboard.com:server");
+const http = require("http");
+const debugAgent = require("@google-cloud/debug-agent");
+debugAgent.start();
 
 const index = require("./routes/index");
 const boards = require("./routes/boards");
 const users = require("./routes/users");
 const profiles = require("./routes/profiles");
 
-const bbcom = express();
-const debug = require("debug")("burnerboard.com:server");
-const http = require("http");
-
-require("@google-cloud/debug-agent").start();
- 
-// uncomment after placing your favicon in /public
-//bbcom.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-bbcom.use(logger("dev"));
 bbcom.use(bodyParser.json());
 bbcom.use(bodyParser.urlencoded({ extended: false }));
 bbcom.use(cookieParser());
 
-bbcom.use(yes({
+bbcom.use(yesHttps({
 	ignoreFilter: (req) => {
 		return ((req.path.endsWith("/DownloadDirectoryJSON") && req.path.startsWith("/boards/")));
 	}
 }));
 
+// react compiled app.
 bbcom.use(express.static("./client/build"));
 bbcom.get("/", function (req, res) {
 	res.redirect("/index.html");
@@ -39,14 +35,14 @@ bbcom.use("/profiles", profiles);
 bbcom.use("/users", users);
 
 // catch 404 and forward to error handler
-bbcom.use(function (req, res, next) {
+bbcom.use((req, res, next) => {
 	var err = new Error("Not Found");
 	err.status = 404;
 	next(err);
 });
 
 // error handler
-bbcom.use(function (err, req, res, next) {
+bbcom.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.bbcom.get("env") === "development" ? err : {};
@@ -62,26 +58,7 @@ bbcom.set("port", port);
 var server = http.createServer(bbcom);
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
-
-function normalizePort(val) {
-	var port = parseInt(val, 10);
-
-	if (isNaN(port)) {
-		// named pipe
-		return val;
-	}
-
-	if (port >= 0) {
-		// port number
-		return port;
-	}
-
-	return false;
-}
-
-function onError(error) {
+server.on("error", (error) => {
 	if (error.syscall !== "listen") {
 		throw error;
 	}
@@ -103,12 +80,32 @@ function onError(error) {
 	default:
 		throw error;
 	}
-}
+});
 
-function onListening() {
+server.on("listening", () => {
 	var addr = server.address();
 	var bind = typeof addr === "string"
 		? "pipe " + addr
 		: "port " + addr.port;
 	debug("Listening on " + bind);
+});
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
 }
+
+
+
+
