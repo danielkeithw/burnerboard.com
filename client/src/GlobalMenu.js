7d44ebbcb25563c2760e78e2c5feacbc4552a2c9
Daniel Wilson <danielkeithw@gmail.com>
2024-11-17 00:11:14 -0800

    add mast to menu

--- client/src/GlobalMenu.js
+++ client/src/GlobalMenu.js
@@ -52,6 +52,7 @@ class GlobalMenu extends React.Component {
 			showProfiles: false,
 			showMedia: false,
 			showMonitors: false,
+			showMasts: false,
 		};
 
 		this.handleInstructions = this.handleInstructions.bind(this);
@@ -142,6 +143,16 @@ class GlobalMenu extends React.Component {
 									key={"board-" + item.board_name}
 									selected={item.board_name === this.props.currentBoard}
 									style={{ display: this.state.showMonitors ? "block" : "none" }}
+								> {item.board_name}
+								</MenuItem>))
+							}
+						</MenuList>
+						<MenuList subheader={<ListSubheader className={classes.listSubheader} disableSticky={true} onClick={event => this.setState({ showMasts: !this.state.showMasts })}>Masts</ListSubheader>} className={classes.list} >
+							{this.props.boardNames.filter((item) => { return item.type === "mast"; }).map(item => (
+								<MenuItem onClick={event => { this.props.onSelectBoard(event, "board-" + item.board_name); this.setState({ showProfiles: true }); }}
+									key={"board-" + item.board_name}
+									selected={item.board_name === this.props.currentBoard}
+									style={{ display: this.state.showMasts ? "block" : "none" }}
 								> {item.board_name}
 								</MenuItem>))
 							}
